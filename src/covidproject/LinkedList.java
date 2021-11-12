329a5c60fa398f4aadfa550923ae828ebf1e6911
lanewills <94198057+lanewills@users.noreply.github.com>
2021-11-12 16:10:48 -0500

    Update LinkedList.java

--- src/covidproject/LinkedList.java
+++ src/covidproject/LinkedList.java
@@ -15,17 +15,17 @@ public class LinkedList<T> implements ListInterface<T> {
 
 	public LinkedList() {
 		initializeDataFields();
-	} // end default constructor
+	}
 
 	public void clear() {
 		initializeDataFields();
-	} // end clear
+	}
 
 	// Initializes the class's data fields to indicate an empty list.
 	private void initializeDataFields() {
 		firstNode = null;
 		numberOfEntries = 0;
-	} // end initializeDataFields
+	}
 	
 	
 	public void add(T newEntry) // OutOfMemoryError possible
@@ -38,10 +38,10 @@ public class LinkedList<T> implements ListInterface<T> {
 		{
 			Node lastNode = getNodeAt(numberOfEntries - 1);
 			lastNode.setNext(newNode); // Make last node reference new node
-		} // end if
+		}
 
 		numberOfEntries++;
-	} // end add
+	}
 
 	public void add(int newPosition, T newEntry) {
 		if ((newPosition >= 0) && (newPosition <= numberOfEntries)) {
@@ -57,12 +57,12 @@ public class LinkedList<T> implements ListInterface<T> {
 				Node nodeAfter = nodeBefore.getNext();
 				newNode.setNext(nodeAfter);
 				nodeBefore.setNext(newNode);
-			} // end if
+			}
 
 			numberOfEntries++;
 		} else
 			throw new IndexOutOfBoundsException("Illegal position given to add operation.");
-	} // end add
+	}
 
 	public T remove(int givenPosition) {
 		T result = null; // Return value
@@ -86,7 +86,7 @@ public class LinkedList<T> implements ListInterface<T> {
 			return result; // Return removed entry
 		} else
 			throw new IndexOutOfBoundsException("Illegal position given to remove operation.");
-	} // end remove
+	}
 
 	public T replace(int givenPosition, T newEntry) {
 		if ((givenPosition >= 0) && (givenPosition < numberOfEntries)) {
@@ -96,14 +96,14 @@ public class LinkedList<T> implements ListInterface<T> {
 			return originalEntry;
 		} else
 			throw new IndexOutOfBoundsException("Illegal position given to replace operation.");
-	} // end replace
+	}
 
 	public T getEntry(int givenPosition) {
 		if ((givenPosition >= 0) && (givenPosition < numberOfEntries)) {
 			return getNodeAt(givenPosition).getData();
 		} else
 			throw new IndexOutOfBoundsException("Illegal position given to getEntry operation.");
-	} // end getEntry
+	}
 
 	public T[] toArray() {
 		// The cast is safe because the new array contains null entries
@@ -116,10 +116,10 @@ public class LinkedList<T> implements ListInterface<T> {
 			result[index] = currentNode.getData();
 			currentNode = currentNode.getNext();
 			index++;
-		} // end while
+		}
 
 		return result;
-	} // end toArray
+	}
 
 	public boolean contains(T anEntry) {
 		boolean found = false;
@@ -130,18 +130,18 @@ public class LinkedList<T> implements ListInterface<T> {
 				found = true;
 			else
 				currentNode = currentNode.getNext();
-		} // end while
+		}
 
 		return found;
-	} // end contains
+	}
 
 	public int getLength() {
 		return numberOfEntries;
-	} // end getLength
+	}
 
 	public boolean isEmpty() {
 		return (numberOfEntries == 0);
-	} // end isEmpty
+	}
 
 	// Returns a reference to the node at a given position.
 	// Precondition: The chain is not empty;
@@ -153,36 +153,36 @@ public class LinkedList<T> implements ListInterface<T> {
 		for (int counter = 0; counter < givenPosition; counter++)
 			currentNode = currentNode.getNext();
 		return currentNode;
-	} // end getNodeAt
+	}
 
 	private class Node {
-		private T data; // Entry in list
-		private Node next; // Link to next node
+		private T data;
+		private Node next;
 
 		private Node(T dataPortion) {
 			data = dataPortion;
 			next = null;
-		} // end constructor
+		}
 
 		private Node(T dataPortion, Node nextNode) {
 			data = dataPortion;
 			next = nextNode;
-		} // end constructor
+		}
 
 		private T getData() {
 			return data;
-		} // end getData
+		}
 
 		private void setData(T newData) {
 			data = newData;
-		} // end setData
+		}
 
 		private Node getNext() {
 			return next;
-		} // end getNextNode
+		}
 
 		private void setNext(Node nextNode) {
 			next = nextNode;
-		} // end setNextNode
-	} // end Node
-} // end LList
+		}
+	}
+}
